c84fdd593058409e1d769b2ce284aefac5eb6f33
27thColt <d27thColt@gmail.com>
2020-05-08 16:11:23 +0800

    Added Inventory Save BUtton

---
+++ Assets/Scripts/UI/SaveButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*  5/8/2020 3:32pm - Save Button
+    Basically just saves the inventory
+
+*/
+public class SaveButton : MonoBehaviour {
+    public void OnSaveButtonPressed() {
+        IO_Inventory.SaveInventory(Inventory.instance.itemInv);
+        Debug.Log("Inventory Saved!");
+    }
+}
