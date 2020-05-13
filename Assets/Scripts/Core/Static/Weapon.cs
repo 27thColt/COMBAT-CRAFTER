2ac0ba6faf338898ce9474811c2b9e934274c9af
27thColt <d27thColt@gmail.com>
2020-05-13 17:25:15 +0800

    Implemented weapons

--- Assets/Scripts/Core/Static/Weapon.cs
+++ Assets/Scripts/Core/Static/Weapon.cs
@@ -5,24 +5,43 @@ using UnityEngine;
 /*  5/10/2020 5:48pm - Weapon Class
     Used for WeaponType items, contains a couple more values
 
+
+    5/11/2020 1:22pm - Honestly really dont feel like my current method for implementing this Weapon subclass is efficient so yeah
+
 */
 public class Weapon : Item {
-    public WeaponType _weaponType;
+    public WeaponType weaponType;
     public int maxDurability;
     public int currentDurability;
-    public Weapon(ItemType _itemType, int _number = 1) : base(_itemType, _number) {
-        _weaponType = _itemType as WeaponType;
 
-        maxDurability = _weaponType.maxDurability;
-        currentDurability = maxDurability;
+
+    public Weapon(ItemType _itemType, int _UID, int _number = 1, int _currentDurability = 0) : base(_itemType, _UID, _number) {
+        weaponType = _itemType as WeaponType;
+
+        maxDurability = weaponType.maxDurability;
+        if (_currentDurability == 0) currentDurability = maxDurability; // When no durability given, just use the max ( 5/11/2020 1:30pm )
+        else currentDurability = _currentDurability;
+
+        // Updates the attack stat ( 5/13/2020 12:37pm )
+        atk = atk - weaponType.atkDecayRate * (maxDurability - currentDurability);
     }
 
-    override public void AddItem(int _amt = 1) {
-        Debug.LogError(_weaponType.name + " is not stackable!");
+    override public void AddInstance(int _amt = 1) {
+        Debug.LogError(weaponType.itemName + " is not stackable!");
     }
     // Removes the durability ( 5/11/2020 10:46am )
-    override public void RemoveItem(int _amt = 1) {
-        currentDurability -= 1;
-        atk = _weaponType.baseAtk - _weaponType.atkDecayRate * _amt;
+    override public bool RemoveInstance(int _amt = 1) {
+        if (currentDurability == 1) {
+            return false;
+        } else {
+            currentDurability -= _amt;
+            atk = atk - weaponType.atkDecayRate * (maxDurability - currentDurability);
+            Debug.Log("Attack of " + itemType.itemName + " is " + atk);
+            return true;
+        }
+    }
+
+    override public void OnCraft() {
+        Inventory.instance.AddItem(this);
     }
 }
